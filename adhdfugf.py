bb105f379911b85bdaa1cfb977019b0b76563aed
Srujan S Govinagidad <srujansgovinagidad@gmail.com>
2026-03-10 00:25:13 +0530

    Update adhdfugf.py

--- adhdfugf.py
+++ adhdfugf.py
@@ -1,10 +1,13 @@
-'''list1 = [1 ,5 ,1]
+#1
+list1 = [1 ,5 ,1]
 copy_list1 = list1.copy()
 copy_list1.reverse()
 if (copy_list1 == list1):
     print("palindrome")
 else:
-    print("Not palindrome") '''
+    print("Not palindrome")
+
+#2
 Tup = ("C","D","A","A","B","B","A")
 print(Tup.count("A"))
 list = ["C","D","A","A","B","B","A"]
