1ff3083519eb08b3c200fde6214631668fca707c
Srujan S Govinagidad <srujansgovinagidad@gmail.com>
2026-03-19 20:40:31 +0530

    Enhance prog2_on_loops.py with new features
    Added functionality to calculate the sum of integers up to n and count letters in a name.

--- prog2_on_loops.py
+++ prog2_on_loops.py
@@ -8,3 +8,25 @@ print ("The end")
 Word = "SRUJAN S GOVINAGIDAD"
 for i in Word:
     print(i, end = ",")
+
+#3 Printing the sum upto n number of integers using for
+n = int(input("enter the number: "))
+sum = 0
+for i in range (n+1):
+    sum = sum + i
+print ("the Sum of numbers from 1 to",n , "is:", sum)    
+
+#4 Counting the numbers of letters in a name 
+Name = input ("Enter the the name: ")
+for i in range (len(Name)):
+    print("the ",i,"letter of Name is",Name[i])
+    
+#5 Find the number in the tuple
+list1 = (1,4,9,16,25,36,49,64,81,100)
+x = int(input("Enter the number to find in list: "))
+i = 0
+print(len(list1))
+while i < len(list1):
+    if (x == list1[i]):
+        print("Found at index: ",i)
+    i += 1
